0ce6ecc6b7b95bc796dcdfc8b053e454774d8331
timoshenko vladimir <>
2025-10-31 22:18:31 +0300

    Углубление в Playwright: Практика по углубленной работе с Playwright

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -1,4 +1,21 @@
-from playwright.sync_api import sync_api, expect
+from playwright.sync_api import sync_playwright,expect
 
-with sync_playwright as playwright():
-    browser = playwrught.chromium.launch(headless = False)
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless = False)
+    page = browser.new_page()
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_disabled()
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    login_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    login_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+    
+    expect(registration_button).to_be_enabled()
+    
